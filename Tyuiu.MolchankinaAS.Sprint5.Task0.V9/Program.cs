c3dac5e620ee0fa93dbb50f39b8d06e6b6b59130
Ангелина Молчанкина <molchankinaap@outlook.com>
2024-12-11 22:14:09 +0500

    Task0

--- Tyuiu.MolchankinaAS.Sprint5.Task0.V9/Program.cs
+++ Tyuiu.MolchankinaAS.Sprint5.Task0.V9/Program.cs
@@ -5,7 +5,28 @@ namespace Tyuiu.MolchankinaAS.Sprint5.Task0.V9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title = "Спринт #5 | Выполнила: Молчанкина А. П. | СМАРТб-24-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спритн #5                                                              *");
+            Console.WriteLine("* Тема: Создания итогого решения по спринту                              *");
+            Console.WriteLine("* Задание #0                                                             *");
+            Console.WriteLine("* Вариант #9                                                             *");
+            Console.WriteLine("* Выполнил: Молчанкина Ангелина Павловна | СМАРТб-24-1                   *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            DataService ds = new DataService();
+            int x = 3;
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл " + res);
+            Console.WriteLine("Cоздан");
+            Console.ReadKey();
         }
     }
 }
