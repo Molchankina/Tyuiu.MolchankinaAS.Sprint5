0de9979e24c70b29b57b836265c17c874873d4de
Ангелина Молчанкина <molchankinaap@outlook.com>
2024-12-11 23:39:20 +0500

    Task1

--- Tyuiu.MolchankinaAS.Sprint5.Task1.V25.Lib/DataService.cs
+++ Tyuiu.MolchankinaAS.Sprint5.Task1.V25.Lib/DataService.cs
@@ -8,45 +8,33 @@ namespace Tyuiu.MolchankinaAS.Sprint5.Task1.V25.Lib
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
-            // Устанавливаем путь к файлу
-            string filePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
-
-            // Создаем текстовый файл
-            using (StreamWriter writer = new StreamWriter(filePath))
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            if (fileExists)
             {
-                writer.WriteLine("x\tF(x)");
+                File.Delete(path);
 
-                // Выводим таблицу на консоль
-                Console.WriteLine("x\tF(x)");
-
-                // Проходим по диапазону значений
-                for (int x = startValue; x <= stopValue; x++)
+            }
+            double y;
+            string str;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = 2 * Math.Sin(x) / (3 * x + 1.2) + Math.Cos(x) - 7 * x * 2;
+                y = Math.Round(y, 2);
+                str = Convert.ToString(y);
+                if (x != stopValue)
                 {
-                    double fx = CalculateFunction(x);
-
-                    fx = Math.Round(fx, 2);
-
-                    writer.WriteLine($"{x}\t{fx.ToString("F2", CultureInfo.InvariantCulture)}");
-                    Console.WriteLine($"{x}\t{fx.ToString("F2", CultureInfo.InvariantCulture)}");
+                    File.AppendAllText(path, str + Environment.NewLine);
                 }
+                else
+                {
+                    File.AppendAllText(path, str);
+                }
+
+
             }
-
-            return filePath;
-        }
-
-        private double CalculateFunction(int x)
-        {
-            // Проверка деления на ноль (если знаменатель равен 0, возвращаем 0)
-            double denominator = 3 * x + 1.2;
-            if (denominator == 0)
-            {
-                return 0;
-            }
-
-            // Вычисляем F(x)
-            double fx = (2 * Math.Sin(x) / denominator) + Math.Cos(x) - 7 * x * 2;
-
-            return fx;
+            return path;
         }
     }
 }
