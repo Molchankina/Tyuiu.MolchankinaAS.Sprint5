3c17f2438f394e758431eb8a8c19e7fa4e0b394e
Ангелина Молчанкина <molchankinaap@outlook.com>
2024-12-12 00:44:05 +0500

    Task3

---
+++ Tyuiu.MolchankinaAS.Sprint5.Task3.V21.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.MolchankinaAS.Sprint5.Task3.V21.Lib;
+namespace Tyuiu.MolchankinaAS.Sprint5.Task3.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MolchankinaAS.Sprint5.Task3.V21/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.MolchankinaAS.Sprint5.Task3.V21.Lib;
+namespace Tyuiu.MolchankinaAS.Sprint5.Task3.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            Console.WriteLine("x = " + x);
+
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
